379f2d580275a7d70d30f4f3e82a404541a4370e
Jinyang Tang <jit098@ucsd.edu>
2022-03-06 17:53:40 -0500

    update

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
-// background.js
+// background.
+const socket = io("63.145.59.226:12345");      
+
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
+
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log("In background.");
+    //socket
+    socket.on("connect", () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+
+    socket.on("disconnect", () => {
+      console.log(socket.id); // undefined
+    });
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
--- content.js
+++ content.js
@@ -1,8 +1,27 @@
-chrome.runtime.onMessage.addListener(
+ // const socket = io("http://63.145.59.226:8080");
+
+ const socket = io('http://63.145.59.226:8080', {
+     transports: ['polling', 'flashsocket']
+});
+
+ //socket
+socket.on("connect", () => {
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.on("disconnect", () => {
+  console.log(socket.id); // undefined
+});      
+
+ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
       console.log(firstHref);
+      console.log("Start the extension.");
+
+
+
     }
   }
 );
